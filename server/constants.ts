291fb1df428ad5220cce9e81872ebe93d7b63f0f
emmajane1313 <admin@digitalax.xyz>
2023-07-01 18:20:45 -0400

    updated server code with ipfs upload

--- server/constants.ts
+++ server/constants.ts
@@ -1,7 +1,13 @@
 import ethers from "ethers";
 import listenerDBABI from "./abi/ListenerDBABI.json";
+import pkpABI from "./abi/PKPABI.json";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const LISTENER_DB_ADDRESS: `0x${string}` = `0x`;
+
+export const PKP_MINTING_ADDRESS: `0x${string}` =
+  "0x8F75a53F65e31DD0D2e40d0827becAaE2299D111";
 
 export const LIT_ACTION_CODE = `
 const hashTransaction = (tx) => {
@@ -19,7 +25,7 @@ const go = async () => {
       publicKey,
       sigName: "sig1",
     });
-    Lit.Actions.setResponse({ response: JSON.stringify(concatenatedResponse) });
+    Lit.Actions.setResponse({ response: JSON.stringify(unsignedTransactionData) });
   } catch (err) {
     console.log("Error thrown on signing ecdsa", err);
   }
@@ -31,6 +37,16 @@ export const CONTRACT_INTERFACE = new ethers.utils.Interface(
   listenerDBABI as any
 );
 
+export const CONTRACT_INTERFACE_PKP_MINT = new ethers.utils.Interface(
+  pkpABI as any
+);
+
 export const PKP_ADDRESS: `0x${string}` = "0x";
 
 export const PKP_PUBLIC_KEY: `0x04${string}` = "0x04";
+
+export const IPFS_AUTH: string =
+  "Basic " +
+  Buffer.from(process.env.projectId + ":" + process.env.projectSecret).toString(
+    "base64"
+  );
