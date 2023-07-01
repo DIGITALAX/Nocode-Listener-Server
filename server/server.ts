b5b448a0c9771f930ccaba18a0ba52d27fd44968
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2023-07-01 18:20:45 -0400

    updated server code with ipfs upload

--- server/server.ts
+++ server/server.ts
@@ -1,38 +1,54 @@
 import express, { Request, Response } from "express";
 import * as LitJsSdk from "@lit-protocol/lit-node-client";
+import bs58 from "bs58";
 import {
   Circuit,
   IConditionalLogic,
   Action,
   Condition,
   IExecutionConstraints,
-  LitUnsignedTransaction,
   ILogEntry,
   LogCategory,
 } from "lit-listener-sdk";
 import { uuid } from "uuidv4";
 import {
   CONTRACT_INTERFACE,
+  CONTRACT_INTERFACE_PKP_MINT,
+  IPFS_AUTH,
   LISTENER_DB_ADDRESS,
   LIT_ACTION_CODE,
   PKP_ADDRESS,
+  PKP_MINTING_ADDRESS,
   PKP_PUBLIC_KEY,
 } from "./constants";
 import { LitAuthSig } from "./types";
+import WebSocket from "ws";
+import http from "http";
+import { create } from "ipfs-http-client";
 
 const activeCircuits = new Map();
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 const port = 3000;
-
+let clientSocket: WebSocket | null = null;
 let authSig: LitAuthSig;
 const litClient = new LitJsSdk.LitNodeClient({
   litNetwork: "serrano",
   debug: false,
 });
+// Create an IPFS client instance
+const client = create({
+  url: "https://ipfs.infura.io:5001/api/v0",
+  headers: {
+    authorization: IPFS_AUTH,
+  },
+});
 
 app.use(express.json());
 
-app.post("/start", async (req: Request, res: Response) => {
+app.post("/instantiate", async (req: Request, res: Response) => {
+  let id: string | undefined;
   try {
     const {
       provider,
@@ -41,13 +57,10 @@ app.post("/start", async (req: Request, res: Response) => {
       contractActions,
       conditionalLogic,
       executionConstraints,
-      signedTransaction,
-      authSignature,
     } = req.body;
 
     // instantiate the new circuit
     const newCircuit = new Circuit(provider, circuitSigner);
-    const instantiatorAddress = await circuitSigner.getAddress();
 
     // Create new circuit with ListenerDB
     const results = await addCircuitLogic(
@@ -55,45 +68,87 @@ app.post("/start", async (req: Request, res: Response) => {
       contractConditions,
       contractActions,
       conditionalLogic,
-      executionConstraints,
-      signedTransaction,
-      instantiatorAddress
+      executionConstraints
     );
+    id = results?.id;
 
     // Add the circuit to the activeCircuits map
-    activeCircuits.set(results?.id, newCircuit);
+    activeCircuits.set(id, {
+      newCircuit,
+      contractConditions,
+      contractActions,
+      conditionalLogic,
+      executionConstraints,
+    });
 
-    if (!results?.publicKey || !authSignature || !results?.id) {
-      // push an error to the websocket to the front end;
-      return;
+    // Send the transaction data to the client for minting and burning the PKP
+    if (clientSocket && results?.ipfsCID) {
+      const txData = generateTransactionDataToSign(results?.ipfsCID as string);
+      clientSocket.send(JSON.stringify(txData));
     }
 
+    res.status(200).json({
+      message: `Circuit Successfully Instantiated with id ${id}`,
+      id,
+    });
+  } catch (err: any) {
+    // Delete on error if it exists
+    if (activeCircuits.has(id)) {
+      activeCircuits.delete(id);
+    }
+    res.json({ message: `Error instantiating Circuit: ${err.message}` });
+  }
+});
+
+app.post("/start", async (req: Request, res: Response) => {
+  try {
+    const { id, instantiatorAddress, authSignature, signedPKPTransactionData } =
+      req.body;
+
+    const { tokenId, publicKey, address } = await activeCircuits
+      .get(id)
+      .newCircuit.mintGrantBurnPKPDatabase(signedPKPTransactionData);
+
+    // Save the circuit configuration to the database
+    await saveCircuitToSubgraph(
+      id,
+      tokenId,
+      publicKey,
+      address,
+      instantiatorAddress,
+      {
+        contractConditions: activeCircuits.get(id).contractConditions,
+        contractActions: activeCircuits.get(id).contractActions,
+        conditionalLogic: activeCircuits.get(id).conditionalLogic,
+        executionConstraints: activeCircuits.get(id).executionConstraints,
+      }
+    );
+
     // Listen in on all logs
-    newCircuit.on("log", (logEntry: ILogEntry) => {
+    activeCircuits.get(id).newCircuit.on("log", (logEntry: ILogEntry) => {
       saveLogToSubgraph(
         logEntry,
-        results?.id as string,
+        id,
         instantiatorAddress,
-        newCircuit
+        activeCircuits.get(id).newCircuit
       );
     });
 
     // Start the circuit
-    const startCircuit = newCircuit.start({
-      publicKey: results?.publicKey,
-      ipfsCID: results?.ipfsCID,
+    const startCircuit = activeCircuits.get(id).newCircuit.start({
+      publicKey: publicKey,
       authSig: authSignature,
     });
 
     setTimeout(() => {
       res.status(200).json({
-        message: `Circuit Successfully Instantiated with id ${results?.id}`,
+        message: `Circuit Successfully Started with id ${id}`,
       });
     }, 20000);
 
     await startCircuit;
   } catch (err: any) {
-    res.json({ message: `Error instantiating Circuit: ${err.message}` });
+    res.json({ message: `Error Starting Circuit: ${err.message}` });
   }
 });
 
@@ -102,11 +157,12 @@ app.post("/interrupt", async (req: Request, res: Response) => {
     const { id, instantiatorAddress } = req.body;
 
     activeCircuits.delete(id);
-    await interruptCircuitRunning(id, instantiatorAddress);
+    const results = await interruptCircuitRunning(id, instantiatorAddress);
 
-    res
-      .status(200)
-      .json({ message: `Circuit Successfully Interrupted with id ${id}` });
+    res.status(200).json({
+      message: `Circuit Successfully Interrupted with id ${id}`,
+      results: JSON.stringify(results),
+    });
   } catch (err: any) {
     res.json({ message: `Error interrupting Circuit: ${err.message}` });
   }
@@ -135,48 +191,25 @@ const addCircuitLogic = async (
   contractConditions: Condition[],
   contractActions: Action[],
   conditionalLogic: IConditionalLogic,
-  executionConstraints: IExecutionConstraints,
-  signedTransaction: LitUnsignedTransaction,
-  instantiatorAddress: string
-): Promise<
-  | {
-      publicKey: string;
-      ipfsCID: string;
-      id: string | undefined;
-    }
-  | undefined
-> => {
+  executionConstraints: IExecutionConstraints
+): Promise<{ id: string; ipfsCID: string | undefined } | undefined> => {
   try {
+    const id = uuid();
     newCircuit.setConditions(contractConditions);
     const litActionCode = newCircuit.setActions(contractActions);
     newCircuit.setConditionalLogic(conditionalLogic);
     newCircuit.executionConstraints(executionConstraints);
-    const ipfsCID = await newCircuit.getIPFSHash(litActionCode);
-    const { publicKey, tokenId, address } = await newCircuit.mintGrantBurnPKP(
-      ipfsCID
-    );
 
-    // Save the circuit configuration to the database
-    const id = await saveCircuitToSubgraph(
-      tokenId,
-      publicKey,
-      address,
-      instantiatorAddress,
-      {
-        contractConditions,
-        contractActions,
-        conditionalLogic,
-        executionConstraints,
-      }
-    );
+    const ipfsCID = await ipfsUpload(litActionCode);
 
-    return { publicKey, ipfsCID, id: id?.id };
+    return { id, ipfsCID };
   } catch (err: any) {
     console.error(err.message);
   }
 };
 
 const saveCircuitToSubgraph = async (
+  id: string,
   tokenId: string,
   pkpPublicKey: string,
   pkpAddress: string,
@@ -187,10 +220,8 @@ const saveCircuitToSubgraph = async (
     conditionalLogic: IConditionalLogic;
     executionConstraints: IExecutionConstraints;
   }
-): Promise<{ id: string; response: string } | undefined> => {
+): Promise<void> => {
   try {
-    const id = uuid();
-
     const circuitInformation = {
       tokenId,
       pkpPublicKey,
@@ -203,9 +234,7 @@ const saveCircuitToSubgraph = async (
       circuitInformation,
       instantiatorAddress,
     ]);
-    const results = await executeJS(unsignedTransactionData);
-
-    return { id, response: results.response };
+    await executeJS(unsignedTransactionData);
   } catch (err: any) {
     console.error(err.message);
   }
@@ -333,4 +362,38 @@ const generateUnsignedData = (functionName: string, args: any[]) => {
     value: 0,
     type: 2,
   };
+};
+
+const generateTransactionDataToSign = (ipfsCID: string) => {
+  return {
+    to: PKP_MINTING_ADDRESS,
+    nonce: 0,
+    chainId: 137,
+    gasLimit: "50000",
+    gasPrice: undefined,
+    maxFeePerGas: undefined,
+    maxPriorityFeePerGas: undefined,
+    data: CONTRACT_INTERFACE_PKP_MINT.encodeFunctionData(
+      "mintGrantAndBurnNext",
+      [2, getBytesFromMultihash(ipfsCID), { value: "1" }]
+    ),
+    value: 0,
+    type: 2,
+  };
+};
+
+const getBytesFromMultihash = (multihash: string): string => {
+  const decoded = bs58.decode(multihash);
+  return `0x${Buffer.from(decoded).toString("hex")}`;
+};
+
+const ipfsUpload = async (
+  litActionCode: string
+): Promise<string | undefined> => {
+  try {
+    const added = await client.add(litActionCode);
+    return added.path;
+  } catch (err: any) {
+    console.error(err.message);
+  }
 };
