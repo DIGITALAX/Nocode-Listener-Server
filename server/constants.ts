87f9562afd13f859cc365f48ab3e7ba43a8e511c
emmajane1313 <admin@digitalax.xyz>
2023-07-21 17:33:05 -0400

    el codigo de server otra vez

--- server/constants.ts
+++ server/constants.ts
@@ -44,6 +44,9 @@ export const CONTRACT_INTERFACE_PKP_MINT = new ethers.utils.Interface(
 export const PKP_PUBLIC_KEY: `0x04${string}` =
   "0x04dce6458f20b315efdf0c04c5acb83561b65949619f66d3e0060b1aeeacc8561d1a424248442bb8a41f27af56146e0f3368ce2a993bdf5cc53b87430f93546c9f";
 
+export const PKP_ETH_ADDRESS: `0x${string}` =
+  "0xE0BE6420194eACB6934d7E151bC0aA45F3c250ED";
+
 export const IPFS_AUTH: string =
   "Basic " +
   Buffer.from(
