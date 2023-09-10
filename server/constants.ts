ea61aec55209e64a98527b32d9739a4d4369c095
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2023-09-10 18:55:51 -0400

    env

--- server/constants.ts
+++ server/constants.ts
@@ -41,14 +41,7 @@ export const CONTRACT_INTERFACE_PKP_MINT = new ethers.utils.Interface(
   pkpABI as any
 );
 
-export const PKP_PUBLIC_KEY: `0x04${string}` =
-  "0x04998334943893c188d8d1eea643dad70ff4e23ff951871e92e722c9eab73386e5835a3c78e2d5a1c9310e274523a2c20729316b96c0bdade6f3d5bead6d760388";
 
-export const PKP_ETH_ADDRESS: `0x${string}` =
-  "0xae415ded71ba8d23748df0f0113e2fbb8225ff10";
-
-export const PKP_TOKEN_ID: string =
-  "8374444442989637949850150452393133218265193208246628247377315071840375026605";
 
 export const IPFS_AUTH: string =
   "Basic " +
